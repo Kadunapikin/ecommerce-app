80989ab5d830493aa85d4cb6c7ce77b61566040a
Kadunapikin <ibakoshi27@gmail.com>
2024-01-10 15:06:08 +0100

    create addProduct function to add a product to cart

--- client/src/context/cartContext.js
+++ client/src/context/cartContext.js
@@ -8,6 +8,25 @@ export const cartProvider = ({children}) => {
 
     const toggleCart = () => setIsOpen(prev => !prev)
 
+    const addProduct = (product) => {
+        const isExisting = products.find((p) => p.id === product.id)
+
+        if (isExisting){
+            setProducts(prev => {
+                const upDatedProducts = prev.map((p) => {
+                    return p.id === product.id
+                    ? {...p, quantity: p.quantity + 1}
+                    : p
+                }) 
+                return upDatedProducts;
+            })
+        } else {
+            setProducts(prev => {
+                return [...prev, product]
+            })
+        }
+    }
+
 }
 
 export const useCartContext = () => {
