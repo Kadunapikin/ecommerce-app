a229380ce43687bc805e0c1295931c0af15d133c
Kadunapikin <ibakoshi27@gmail.com>
2024-01-10 15:04:15 +0100

    add cartContext.js to contex dir and import some states

---
+++ client/src/context/cartContext.js
@@ -0,0 +1,15 @@
+import { useContext, createContext, useState } from "react";
+
+const cartContext = createContext();
+
+export const cartProvider = ({children}) => {
+    const [products, setProducts] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleCart = () => setIsOpen(prev => !prev)
+
+}
+
+export const useCartContext = () => {
+    return useContext(useCartContext);
+}
