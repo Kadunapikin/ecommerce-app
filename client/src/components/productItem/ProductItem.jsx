ccdf4b30db41945534f6ad88e2b8253808a79b69
Kadunapikin <ibakoshi27@gmail.com>
2024-01-10 15:52:54 +0100

    add product details on ProductItem.jsx

--- client/src/components/productItem/ProductItem.jsx
+++ client/src/components/productItem/ProductItem.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import classes from './productItem.module.css';
+import { products } from '../../data/data';
+import { useCartContext } from '../../context/cartContext';
 
 const Products = () => {
+    const {addProduct} = useCartContext
+
   return (
     <div className={classes.container}>
         {products.map((product) => {
-            <div key={product.id}>
-
+            <div key={product.id} className={classes.product}>
+                <img src={product.img} alt="" />
+                <div className={classes.productDetails}>
+                    <h3>{product.name}</h3>
+                    <span>${product.price}</span>
+                </div>
+                <button onClick={() => addProduct(product)}>Add to Cart</button>
             </div>
         })}
     </div>
