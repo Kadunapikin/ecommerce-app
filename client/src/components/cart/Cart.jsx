fceed3a7309d2aa9b44116018ffe9f6435c2d15b
Kadunapikin <ibakoshi27@gmail.com>
2024-01-10 18:02:07 +0100

    update and style Cart.jsx

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import classes from './cart.module.css';
+import { useCartContext } from '../../context/cartContext';
+import {AiOutlineShoppingCart} from 'react-icons/ai';
 
 const Cart = () => {
+  const {products, isOpen, toggleCart, removeProduct} = useCartContext();
+
   return (
-    <div>Cart</div>
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.cartIcon} onClick={toggleCart}>
+          <AiOutlineShoppingCart size={25} />
+          <span className={classes.cartNumber}>
+            {products?.length}
+          </span>
+        </div>
+      </div>
+      Cart
+    </div>
   )
 }
 
