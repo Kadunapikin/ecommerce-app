e4562a0ee30a85a28590b74bcae8b3fda032d717
Kadunapikin <ibakoshi27@gmail.com>
2024-01-11 09:07:04 +0100

    add middlewares

--- server/index.js
+++ server/index.js
@@ -4,3 +4,7 @@ const cors = require('cors');
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 const app = express();
 
+//MIDLEWARES
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
