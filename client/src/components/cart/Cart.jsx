f409243ad55510c61023ca085af5519f511a1e15
Kadunapikin <ibakoshi27@gmail.com>
2024-01-11 07:55:10 +0100

    add jsx items to Cart.jsx and style them

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './cart.module.css';
 import { useCartContext } from '../../context/cartContext';
-import {AiOutlineShoppingCart} from 'react-icons/ai';
+import {AiOutlineClose, AiOutlineShoppingCart} from 'react-icons/ai';
 
 const Cart = () => {
   const {products, isOpen, toggleCart, removeProduct} = useCartContext();
@@ -9,11 +9,36 @@ const Cart = () => {
   return (
     <div className={classes.container}>
       <div className={classes.wrapper}>
-        <div className={classes.cartIcon} onClick={toggleCart}>
-          <AiOutlineShoppingCart size={25} />
+        <div className={classes.cartIcon}>
+          <AiOutlineShoppingCart size={25} onClick={toggleCart} />
           <span className={classes.cartNumber}>
             {products?.length}
           </span>
+          {isOpen && (
+            <div className={classes.cartContainer}>
+              {products?.length > 0 ? (
+                <>
+                <h4>Products</h4>
+                <div className={classes.productContainer}>
+                  {products.map((product) => (
+                    <div className={classes.product} key={product.id}>
+                      <img src={product.img} alt="" />
+                      <div className={classes.productDetails}>
+                        <h3>{product.name}</h3>
+                        <span>{product.quantity} x ${product.price}</span>
+                      </div>
+                      <AiOutlineClose onClick={() => removeProduct(product)} />
+                    </div>
+                  ))}
+                </div>
+                <div className={classes.controls}>
+                  <button onClick={() => {}}>Checkout</button>
+                  <span onClick={toggleCart}>Close Cart</span>
+                </div>
+                </>
+              ) : (<h3>No Product in Cart</h3>)}
+            </div>
+          )}
         </div>
       </div>
       Cart
