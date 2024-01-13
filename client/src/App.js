161ff3713a6410959b7b4348c5d3f7a3f6d9646e
Kadunapikin <ibakoshi27@gmail.com>
2024-01-13 12:42:23 +0100

    integrate login and register component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+// App.js
 import './App.css';
 import Cart from './components/cart/Cart';
 import ProductItem from './components/productItem/ProductItem';
 import Success from './components/success/Success';
 import { Routes, Route } from 'react-router-dom';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
 
 function App() {
   return (
@@ -10,11 +13,13 @@ function App() {
       <Routes>
         <Route path='/' element={
           <>
-          <Cart />
-          <ProductItem />
+            <Cart />
+            <ProductItem />
           </>
-        }  />
-        <Route path='/success' element={<Success />}  />
+        } />
+        <Route path='/success' element={<Success />} />
+        <Route path='/login' element={<Login />} /> {/* Add Login route */}
+        <Route path='/register' element={<Register />} /> {/* Add Register route */}
       </Routes>
     </div>
   );
